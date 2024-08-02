49abee1e429ea67a89a163d5c71ebb6eb056e636
Narsars <109426437+Narsars@users.noreply.github.com>
2024-08-02 11:53:32 +0800

    Add files via upload
    v 0.001-01-0802

---
+++ questions.js
@@ -0,0 +1,13 @@
+const questions = [
+  {
+    question: "世界貿易組織（WTO）取代了關貿總協（GATT）成為推動與規範全球自由貿易的新組織？,
+    options: ["O", "X"],
+    correctAnswer: "O"
+  },
+  {
+    question: "台灣於2002年1月1日正式成為WTO會員國？,
+    options: ["O", "X"],
+    correctAnswer: "O"
+  },
+  // ... 更多問題 ...
+];
